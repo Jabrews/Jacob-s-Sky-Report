e00ac6ee922866756e5fcdac878b3d57b2fcd8b4
Jacob Brewbaker <brewbakerjacob@gmail.com>
2024-07-23 10:42:56 -0400

    finished nav, decided to create better branch system

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,17 @@
+const hamburger_icon = document.querySelector('#menu-icon')
+const nav_menu = document.querySelector('.mobile-navbar')
+
+
+function showNavMenu() {
+    if (nav_menu.style.display === 'none') {
+        nav_menu.style.display = 'block';
+    } else {
+        nav_menu.style.display = 'none';
+    }
+
+}
+
+
 async function initMap() {
 
     const options = {
@@ -23,4 +37,30 @@ async function initMap() {
 
 };
 
-initMap();
+
+function curSuccess(pos) {
+    const coord = pos.coords;
+    
+
+    console.log(`Latitude: ${coord.latitude}, longitude: ${coord.longitude}`)
+}
+
+function curError(err) {
+    console.log(`error:' ${err.code}`)
+}
+
+const curOptions = { 
+    enableHighAccuracy: true, // use gps if available
+    timeout : 5000, // wait and stop looking for pos
+    maximumAge: 0 //do not use catched pos
+
+}
+
+function init() {
+    initMap()
+    hamburger_icon.addEventListener('click', showNavMenu)
+    navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
